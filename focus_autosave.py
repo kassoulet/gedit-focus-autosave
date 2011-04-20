a613ffa74f0cc969ab23c913c0cff22d0ee900e2
gautier <gautier@quad-damage.(none)>
2011-04-20 22:49:06 +0200

    Simplify code. Fix #3, thanks nacho.

--- focus_autosave.py
+++ focus_autosave.py
@@ -6,21 +6,13 @@
 
 from gi.repository import GObject, Gtk, Gdk, Gedit
 
-class AutosaveWindow:
-    def __init__(self, plugin, window):
-        self.window = window
-        self.plugin = plugin
+class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "FocusAutoSavePlugin"
 
-        self.window.connect("focus-out-event", self.on_focus_out_event)
+    window = GObject.property(type=Gedit.Window)
 
-    def deactivate(self):
-        self.window = None
-        self.plugin = None
-
-    def update_ui(self):
-        # Called whenever the window has been updated (active tab
-        # changed, etc.)
-        pass
+    def __init__(self):
+        GObject.Object.__init__(self)
 
     def on_focus_out_event(self, widget, focus):
         for doc in self.window.get_unsaved_documents():
@@ -30,24 +22,14 @@ class AutosaveWindow:
                 continue
             doc.save(0)
 
-
-class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
-    __gtype_name__ = "FocusAutoSavePlugin"
-
-    window = GObject.property(type=Gedit.Window)
-
-    def __init__(self):
-        GObject.Object.__init__(self)
-        self.instances = {}
-
     def do_activate(self):
-        self.instances[self.window] = AutosaveWindow(self, self.window)
+        self.signal = self.window.connect("focus-out-event", self.on_focus_out_event)
 
     def do_deactivate(self):
-        self.instances[self.window].deactivate()
-        del self.instances[self.window]
+        self.window.disconnect(self.signal)
+        del self.signal
 
     def do_update_state(self):
-        self.instances[self.window].update_ui()
+        pass
 
 
