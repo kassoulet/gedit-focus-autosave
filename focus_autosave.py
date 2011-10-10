e84e4de339838d9e65ee57e61c0c806c1cb6cd8f
gautier <gautier.portet@vegatechnologies.fr>
2011-10-10 13:12:49 +0200

    try toi work on gedit3?

--- focus_autosave.py
+++ focus_autosave.py
@@ -4,7 +4,8 @@
 # Save document when losing focus.
 # Gautier Portet <kassoulet gmail.com>
 
-from gi.repository import GObject, Gtk, Gdk, Gedit
+from gi.repository import GObject, Gtk, Gdk, Gedit, Gio
+import datetime
 
 class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
     __gtype_name__ = "FocusAutoSavePlugin"
@@ -15,13 +16,33 @@ class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
         GObject.Object.__init__(self)
 
     def on_focus_out_event(self, widget, focus):
+        #print '\n'.join(dir(doc))
+        #self.tab.set_state(Gedit.TabState.SAVING)
+        #self.window.do_saving()
         for doc in self.window.get_unsaved_documents():
+            #print '\n'.join(dir(doc))
             if doc.is_untouched():
                 continue
-            if doc.get_location() is None:
+            if doc.is_untitled():
                 continue
-            doc.save(0)
+                now = datetime.datetime.now();
+                filename = now.strftime("/tmp/gedit.unsaved.%Y%m%d-%H%M%S.txt");
+                doc.set_location(Gio.file_parse_name(filename))
+            tab = Gedit.Tab.get_from_document(doc)
+            #doc.emit('save')
+            tab.emit('save')
+            #doc.save(0)
+            #print '\n'.join(dir(Gedit.Tab))
+            #autosave = tab.get_auto_save_enabled()
+            #autosave_interval = tab.get_auto_save_interval()
+            #tab.set_auto_save_enabled(True)
+            #tab.set_auto_save_interval(0)
+                        
 
+    def reset_auto_save(self):
+        pass
+            
+            
     def do_activate(self):
         self.signal = self.window.connect("focus-out-event", self.on_focus_out_event)
 
