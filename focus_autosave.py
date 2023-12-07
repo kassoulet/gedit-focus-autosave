56064a7b2e8eff91f49baec2ce050cecfe3cb005
javad <javadr@gmail.com>
2023-12-07 17:08:46 +0330

    (upd) replacing os with pathlib

--- focus_autosave.py
+++ focus_autosave.py
@@ -4,19 +4,13 @@
 # Save document when losing focus.
 # Gautier Portet <kassoulet gmail.com>
 
-from gi.repository import GObject, Gedit, Gio
 import datetime
-import os
+
+from gi.repository import GObject, Gedit, Gio
+from pathlib import Path
 
 # You can change here the default folder for unsaved files.
-dirname = os.path.expanduser("~/.gedit_unsaved/")
-
-
-def assure_path_exists(path):
-    dir = os.path.dirname(path)
-    if not os.path.exists(dir):
-            os.makedirs(dir)
-
+dirname = Path("~/.gedit_unsaved/").expanduser()
 
 class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
     __gtype_name__ = "FocusAutoSavePlugin"
@@ -36,8 +30,8 @@ class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
             if doc.get_file().get_location() is None:
                 # provide a default filename
                 now = datetime.datetime.now()
-                assure_path_exists(dirname)
-                filename = now.strftime(dirname + "%Y%m%d-%H%M%S-%%d.txt") % (n + 1)
+                Path(dirname).mkdir(parents=True, exist_ok=True)
+                filename = str(dirname/now.strftime(f"%Y%m%d-%H%M%S-{n+1}.txt"))
                 doc.get_file().set_location(Gio.file_parse_name(filename))
             # save the document
             Gedit.commands_save_document(self.window, doc)
