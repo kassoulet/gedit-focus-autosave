28a80f99f2e72dbb1b7c5d1e74b1ade6bda61e93
javad <javadr@gmail.com>
2023-12-07 17:23:03 +0330

    (upd) Prevent saving in `.gedit_unsaved` when Ctrl+S is pressed!

--- focus_autosave.py
+++ focus_autosave.py
@@ -6,11 +6,18 @@
 
 import datetime
 
-from gi.repository import GObject, Gedit, Gio
+from gi.repository import GObject, Gedit, Gio, Gdk
 from pathlib import Path
+
+def on_key_press(widget, event):
+  global Ctrl_S
+  if event.state == Gdk.ModifierType.CONTROL_MASK and event.keyval == Gdk.KEY_s:
+      Ctrl_S = True
 
 # You can change here the default folder for unsaved files.
 dirname = Path("~/.gedit_unsaved/").expanduser()
+
+Ctrl_S = False
 
 class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
     __gtype_name__ = "FocusAutoSavePlugin"
@@ -20,6 +27,11 @@ class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
         GObject.Object.__init__(self)
 
     def on_focus_out_event(self, widget, focus):
+        global Ctrl_S
+        if Ctrl_S:
+            # skip to user specified file name
+            Ctrl_S = False
+            return
         for n, doc in enumerate(self.window.get_unsaved_documents()):
             if doc.is_untouched():
                 # nothing to do
@@ -38,10 +50,12 @@ class FocusAutoSavePlugin(GObject.Object, Gedit.WindowActivatable):
 
     def do_activate(self):
         self.signal = self.window.connect("focus-out-event", self.on_focus_out_event)
+        self.ctrl_s = self.window.connect("key-press-event", on_key_press)
 
     def do_deactivate(self):
         self.window.disconnect(self.signal)
-        del self.signal
+        self.window.disconnect(self.ctrl_s)
+        del self.signal, self.ctrl_s
 
 
 
